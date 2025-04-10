ab20a23995e984e4c519ee8003c96b38ca8bd187
Enixrage <123439128+Enixrage@users.noreply.github.com>
2025-04-10 23:59:09 +0000

    more cards

--- falshcards.js
+++ falshcards.js
@@ -19,6 +19,55 @@ export const flashcards = [
       dealerUpcard: "7",
       strategy: "Hit",
       hint: "13 vs 7 is dangerous. You need a stronger total."
-    }
+    },
+    {
+      id: 4,
+      playerTotal: "8",
+      dealerUpcard: "5",
+      strategy: "Hit",
+      hint: "Always hit when below 8 - hand is not strong enough."
+    },
+    {
+      id: 5,
+      playerTotal: "10",
+      dealerUpcard: "9",
+      strategy: "Double down",
+      hint: "You hold advantage, get im."
+    },
+    {
+      id: 6,
+      playerTotal: "6,6",
+      dealerUpcard: "3",
+      strategy: "Split",
+      hint: "Split to get a better hand."
+    },
+    {
+      id: 7,
+      playerTotal: "9,9",
+      dealerUpcard: "7",
+      strategy: "Stand",
+      hint: "18 is strong - 7 is dangerous"
+    },
+    {
+      id: 8,
+      playerTotal: "A,4",
+      dealerUpcard: "4",
+      strategy: "Double down",
+      hint: "Double against weak upcard - soft 15 can be better."
+    },
+    {
+      id: 9,
+      playerTotal: "A,6",
+      dealerUpcard: "2",
+      strategy: "Hit",
+      hint: "Soft 17 vs. strong upcard - try to get a better hand."
+    },
+    {
+      id: 10,
+      playerTotal: "8,8",
+      dealerUpcard: "10",
+      strategy: "Split",
+      hint: "16 is the worst hand - always split."
+    },
   ];
   
