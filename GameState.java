38da589a4f66b98c2a992b92caf9c6e12774c66c
HamidYassin <hamidcaldero@gmail.com>
2025-04-09 20:53:40 -0400

    Update GameState.java

--- GameState.java
+++ GameState.java
@@ -1,24 +1,19 @@
-import java.util.ArrayList;
-
 public class GameState {
     private Deck deck;
-    private ArrayList<Player> players; // List of players (up to 5)
+    private Player player;
     private Player dealer;
     private int round;
 
     public GameState() {
         this.deck = new Deck();
-        this.players = new ArrayList<>();
+        this.player = new Player();
         this.dealer = new Player();
         this.round = 1;
-        
-        for (int i = 0; i < 5; i++) {
-            players.add(new Player()); //creates up to 5 users to play
-        }
     }
 
+    // Getters and Setters
     public Deck getDeck() { return deck; }
-    public ArrayList<Player> getPlayers() { return players; }
+    public Player getPlayer() { return player; }
     public Player getDealer() { return dealer; }
     public int getRound() { return round; }
 
@@ -28,10 +23,9 @@ public class GameState {
 
     public void reset() {
         this.deck = new Deck();
+        this.player.resetHand();
         this.dealer.resetHand();
         this.round = 1;
-        for (Player player : players) {
-            player.resetHand();
-        }
     }
 }
+
